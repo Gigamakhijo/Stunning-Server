e83b6d971de9f528736b7f3be03ec5b7bbbb0dba
dldnfla <lwr0221716@naver.com>
2023-11-18 00:31:08 +0900

    create auth crud

--- app/crud/auth.py
+++ app/crud/auth.py
@@ -1,27 +1,6 @@
-from sqlalchemy.orm import Session
 from passlib.context import CryptContext
-
-from .. import models, schemas
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
-def get_user(db: Session, id: int):
-    return db.query(schemas.User).filter(schemas.User.id == id).first()
-
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(schemas.User).filter(schemas.User.email == email).first()
-
-
 def get_password_hash(plain_password):
     return pwd_context.hash(plain_password)
-
-
-def create_user(db: Session, user: models.User):
-    hashed_password = get_password_hash(user.hashed_password)
-    db_user = schemas.User(email=user.email, hashed_password=hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
