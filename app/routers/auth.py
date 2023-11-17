b88539f73c5b3eb1311e5add26da1ad582399c43
urw7rs <urw.7rs@icloud.com>
2023-11-17 20:28:35 +0900

    separate directories

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
-from ..cruds import auth_crud
+from .. import crud
 
 from .. import models, schemas
 from ..database import SessionLocal, engine
@@ -22,15 +22,15 @@ def get_db():
 
 @router.post("/users/", response_model=models.User)
 def register_user(user: models.User, db: Session = Depends(get_db)):
-    db_user = auth_crud.get_user_by_email(db, email=user.email)
+    db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return auth_crud.create_user(db=db, user=user)
+    return crud.create_user(db=db, user=user)
 
 
 @router.get("/users/{user_id}", response_model=models.User)
 def get_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = auth_crud.get_user(db, id=user_id)
+    db_user = crud.get_user(db, id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
