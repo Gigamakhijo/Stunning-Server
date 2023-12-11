2f28de2b12f2425a5ffbf9640fd489ea40f27d88
urw7rs <urw.7rs@icloud.com>
2023-12-11 22:39:26 +0900

    fix tests

--- app/tests/test_users_route.py
+++ app/tests/test_users_route.py
@@ -83,9 +83,10 @@ def test_update_user_success(authorized_client, email):
 
 
 def test_user_search(authorized_client, other_test_user):
-    print(other_test_user)
     response = authorized_client.get(f"/users/{other_test_user['username']}")
     assert response.status_code == 200, response.text
 
     data = response.json()
-    assert len(data) == 1
+
+    for key in data:
+        assert data[key] == other_test_user[key]
