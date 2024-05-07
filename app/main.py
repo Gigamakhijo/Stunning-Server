050821d4b3670452148173aaacf521b2246e5325
urw7rs <urw.7rs@icloud.com>
2024-05-07 14:42:40 +0900

    enable only todo routes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
-from .routers import challenges, comments, todos
+from .routers import todos
 
 app = FastAPI()
 
 app.include_router(todos.router)
-app.include_router(challenges.router)
-app.include_router(comments.router)
